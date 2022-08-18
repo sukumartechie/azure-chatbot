3faa0bfccbfaadd40afa6006100adc2e50067f25
sukumartechie <109282089+sukumartechie@users.noreply.github.com>
2022-08-18 12:24:40 +0530

    Azure faq bot

---
+++ azuredemobotfaqs-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\demobotforazure-bot-b2c7-Web-Deploy.pubxml -p:Password=ErcMQuteYftL401ehGpNzigKMKlPLTPcxZexwuqJmdAjAPMvjpaeT89PCG1v
+
